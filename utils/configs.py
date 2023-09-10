f198ffb8cd4f502b4918a4848c7170754865e0ac
LMaruizofc <57048561+LMaruizofc@users.noreply.github.com>
2023-09-10 11:53:52 -0300

    Add files via upload

---
+++ utils/configs.py
@@ -0,0 +1,59 @@
+configData = {
+
+  "token": "",
+
+  "mongokey": "",
+
+  "prefix": "",
+
+  "guild": 0,
+  
+  "roles":{
+
+    "staff": {
+
+      "admin": 0,
+
+      "mod": 0,
+
+      "suporte": 0,
+
+      "staff": 0
+
+    },
+
+    "adv": {
+
+      "adv1": 0,
+
+      "adv2": 0,
+
+      "adv3": 0
+
+    },
+
+    "outras":{
+
+      "standby": 0,
+
+      "mute": 0,
+
+    }
+
+  },
+
+  "chats":{
+
+    "ausencia": 0,
+
+    "cmdstf": 0,
+
+    "chatsuporte": 0
+
+  },
+  
+  "catego":{
+    "ticket": 0
+  }
+
+}
